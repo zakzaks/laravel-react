868272563a7903e80cd410da9940dc64aaddae2b
zakzaks <ariefn82@gmail.com>
2025-08-09 09:46:43 +0700

    feat: added person model, controller, resource, migration

---
+++ app/Models/Person.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    protected $fillable = [
+        'name',
+        'address',
+        'phone',
+        'birth',
+        'gender',
+        'photo',
+    ];
+}
--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { BookOpen, Folder, LayoutGrid, User } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -12,6 +12,11 @@ const mainNavItems: NavItem[] = [
         title: 'Dashboard',
         href: '/dashboard',
         icon: LayoutGrid,
+    },
+    {
+        title: 'Person',
+        href: '/person',
+        icon: User,
     },
 ];
 
